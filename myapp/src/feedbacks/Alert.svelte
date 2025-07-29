bab51a49a01fd2f406ba691b522a5373b96ecb82
Kp270705 <kunalpathak4774@gmail.com>
2025-07-30 00:06:28 +0530

    Initial commit

---
+++ myapp/src/feedbacks/Alert.svelte
@@ -0,0 +1,3 @@
+<script>
+    import Alert from "flowbite-svelte/Alert.svelte";
+</script>
---
+++ myapp/src/jsContent/cardContent.js
@@ -0,0 +1,31 @@
+import Watch from "../assets/images/watch1.webp";
+import Ipad from "../assets/images/ipad1.jpeg";
+import IpadAir from "../assets/images/ipadAir1.jpeg";
+import Iphone from "../assets/images/iphone1.jpeg";
+
+export const eComData = [
+  {
+    img: Watch,
+    title: "Apple Watch 7",
+    rating: 4,
+    price: 599
+  },
+  {
+    img: Ipad,
+    title: "iPad",
+    rating: 5,
+    price: "$349"
+  },
+  {
+    img: IpadAir,
+    title: "iPad Air",
+    rating: 5,
+    price: "$599"
+  },
+  {
+    img: Iphone,
+    title: "iPhone 15",
+    rating: 5,
+    price: "$699"
+  }
+];
